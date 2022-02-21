e3181cb7b4a7843640bfbd2cb3e671def7da76dd
Javi Reyes <javireyes26@gmail.com>
2022-02-20 18:39:13 -0600

    Alta del proyecto en github

--- users/models.py
+++ users/models.py
@@ -2,4 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
