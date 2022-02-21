875520c0441cb6f00adb1bbc6aeee4c46dcb50e5
Javi Reyes <javireyes26@gmail.com>
2022-02-21 02:34:24 -0600

    Se genero el registro de usuarios con password encriptado

--- users/api/serializer.py
+++ users/api/serializer.py
@@ -5,3 +5,11 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'username', 'password', 'saldo']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
