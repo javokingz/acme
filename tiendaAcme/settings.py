e063cf7e9aa52911d65fec5b001d43400052c58f
Javi Reyes <javireyes26@gmail.com>
2022-02-21 02:46:31 -0600

    Creación del login

--- tiendaAcme/settings.py
+++ tiendaAcme/settings.py
@@ -117,6 +117,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+REST_FRAMEWORK = {
+    
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+    
+}
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
@@ -130,4 +139,6 @@ STATIC_ROOT = './static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+
 
