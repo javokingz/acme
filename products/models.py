cb4731489ebd2ee90796658224eec91b6a6773fe
Javi Reyes <javireyes26@gmail.com>
2022-02-22 00:16:14 -0600

    Se agregaron el model de product

---
+++ products/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.db.models import SET_NULL
+from users.models import User
+from categories.models import Category
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
--- tiendaAcme/settings.py
+++ tiendaAcme/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     #Apps del proyecto
     'users',
     'categories',
+    'products',
     
 ]
 
