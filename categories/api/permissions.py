8e098c6e502f655e14904fcaeca207c140b9d13e
Javi Reyes <javireyes26@gmail.com>
2022-02-21 19:31:02 -0600

    Se agregaron permisos y get por el name de la categoria

--- categories/api/permissions.py
+++ categories/api/permissions.py
@@ -1,4 +1,8 @@
-# form rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission
 
-# class IsAdminOrNot(BasePermission):
-#     pass
+class IsAdminOrNot(BasePermission):
+    def has_permission(self, request, view):
+        if request.method == 'GET':
+            return True
+        else:
+            return request.user.is_staff
--- categories/api/views.py
+++ categories/api/views.py
@@ -1,10 +1,13 @@
+from cgitb import lookup
 from rest_framework.viewsets import ModelViewSet
 #from django_filters.rest_framework import DjangoFilterBackend
 
 from categories.models import Category
 from categories.api.serializers import CategorySerializer
-
+from categories.api.permissions import IsAdminOrNot
 
 class CategoryApiViewSet(ModelViewSet):
+    permission_classes = [IsAdminOrNot]
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+    lookup_field = 'name'
